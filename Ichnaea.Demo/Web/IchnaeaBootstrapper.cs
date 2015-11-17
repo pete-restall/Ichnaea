d17d169a39d8498397973bd8d849848b0454494d
pete-restall <pete@restall.net>
2015-11-17 11:44:54 +0000

    Some IchnaeaBootstrapper hacks to get the Demo application to run.

--- Ichnaea.Demo/Web/IchnaeaBootstrapper.cs
+++ Ichnaea.Demo/Web/IchnaeaBootstrapper.cs
@@ -10,20 +10,32 @@ namespace Restall.Ichnaea.Demo.Web
 		public static void RegisterRequestScopeIchnaeaDependenciesInto(TinyIoCContainer container)
 		{
 			// TODO: PROPER FLUENT / CONVENTION-BASED CONFIGURATION
-			container.Register((ctx, _) => CreateDomainEventStream(ctx.Resolve<NEventStoreSession>(), new NamedPropertyAggregateRootIdGetter<Account, AccountId>("Id")));
+			container.RegisterAggregateRoot(new NamedPropertyAggregateRootIdGetter<Account, AccountId>("Id"));
+		}
+
+		private static void RegisterAggregateRoot<TAggregateRoot, TAggregateRootId>(
+			this TinyIoCContainer container, IAggregateRootIdGetter<TAggregateRoot, TAggregateRootId> idGetter)
+			where TAggregateRoot : class
+		{
+			var postPersistenceDomainEventTracker = new LambdaPostPersistenceDomainEventTracker<TAggregateRoot>();
+			var prePersistenceDomainEventTracker = new InMemoryDomainEventTracker<TAggregateRoot>(postPersistenceDomainEventTracker);
+			container.Register((ctx, _) => CreateDomainEventStream(ctx.Resolve<NEventStoreSession>(), idGetter, prePersistenceDomainEventTracker, postPersistenceDomainEventTracker));
+			container.Register<IDomainEventTracker<TAggregateRoot>>(prePersistenceDomainEventTracker);
 		}
 
 		private static IDomainEventStream<TAggregateRoot, TAggregateRootId> CreateDomainEventStream<TAggregateRoot, TAggregateRootId>(
-			IStoreEvents session, IAggregateRootIdGetter<TAggregateRoot, TAggregateRootId> idGetter)
+			IStoreEvents session,
+			IAggregateRootIdGetter<TAggregateRoot, TAggregateRootId> idGetter,
+			IPrePersistenceDomainEventTracker<TAggregateRoot> prePersistenceDomainEventTracker,
+			IPostPersistenceDomainEventTracker<TAggregateRoot> postPersistenceDomainEventTracker)
 			where TAggregateRoot: class
 		{
-			var postPersistenceDomainEventTracker = new LambdaPostPersistenceDomainEventTracker<TAggregateRoot>();
 			string bucketId = typeof(TAggregateRoot).Name;
 			return new DomainEventStreamAdapter<TAggregateRoot, TAggregateRootId>(
 				new PersistedEventStreamCreator<TAggregateRoot>(
 					session,
 					bucketId,
-					new InMemoryDomainEventTracker<TAggregateRoot>(postPersistenceDomainEventTracker),
+					prePersistenceDomainEventTracker,
 					aggregateRoot => idGetter.GetIdFrom(aggregateRoot).ToString(), domainEvent => new EventMessage { Body = domainEvent }),
 				new PersistedEventStreamOpener<TAggregateRoot, TAggregateRootId>(
 					session,
